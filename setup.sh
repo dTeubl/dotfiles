a3e675f3a11a133ce464c8934945c1f957a0db81
Dani Teubl <teubl.dani@gmail.com>
2017-09-10 21:16:31 +0200

    backup already existing file

--- setup.sh
+++ setup.sh
@@ -2,4 +2,25 @@
 
 # Simple script to create a simlink from each config file to ~/.* files.
 
+BACKUP_DIR=./backup_old_configs
+
+# Create temporary diretory for the old configuration files
+mkdir -p $BACKUP_DIR
+
+LINKED_FILE=vimrc
+OLD_FILE=~/.$LINKED_FILE
+
+# Link the vimrc, but create a backup before
+if [ -f $OLD_FILE ]; then
+    echo "There is already a vimrc @HOME!"
+    if [ -L $OLD_FILE ]; then
+        echo "It is also a symlink!"
+        var=`ls -l $OLD_FILE | gawk '{ print $11}' | gawk '{ gsub (/[\/.]/, "");print}'`
+        cp $OLD_FILE "./backup_old_configs/vimrc_old_at_$var"
+    else
+        cp $OLD_FILE "./backup_old_configs/vimrc_old"
+    fi
+fi
+
+# ln -s ./vimrc $OLD_FILE
 
