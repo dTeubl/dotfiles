10111856afde3ff5c9dd3c5d7e1bc8278075894e
Dani Teubl <teubl.dani@gmail.com>
2017-09-10 22:01:15 +0200

    Backup fine for all files in ./conf_files

---
+++ clean_backup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# clean the backup directory
+rm -v ./backup_old_configs/*
--- setup.sh
+++ setup.sh
@@ -7,30 +7,40 @@ BACKUP_DIR=./backup_old_configs
 # Create temporary diretory for the old configuration files
 mkdir -p $BACKUP_DIR
 
+# change directory to get better file names
+OLD_PATH=$PWD
+cd ./conf_files
+
 # Create a backup from every config file which will be modified
 for file in *
 do
     if [[ -f $file ]]; then
-        echo $file
-
         OLD_FILE=~/.$file
 
-        # Link the vimrc, but create a backup before
+        # echo $OLD_FILE
+        # Create a backup file from the already existing file
         if [ -f $OLD_FILE ]; then
-            echo "There is already a vimrc @HOME!"
+            echo "There is already a $file @HOME! Creating Backup..."
             if [ -L $OLD_FILE ]; then
-                echo "It is also a symlink!"
-                var=`ls -l $OLD_FILE | gawk '{ print $11}' | gawk '{ gsub (/[\/.]/, "");print}'`
-                cp $OLD_FILE "./backup_old_configs/vimrc_old_at_$var"
+                # echo "It is also a symlink!"
+                var=`ls -l $OLD_FILE | gawk '{ print $11}' | gawk '{ gsub (/[\/.]/, "_");print}'`
+                # echo $var
+                cp $OLD_FILE "$OLD_PATH/backup_old_configs/old_$file$var"
             else
-                cp $OLD_FILE "./backup_old_configs/vimrc_old"
+                cp $OLD_FILE "$OLD_PATH/backup_old_configs/old_$file"
             fi
 
-            # Remove the old config file
+
+            # Remove the old file
             # rm $file
         fi
         
         # create a symbolic link to the ~/directory
         # ln -s $file $OLD_FILE
     fi
+    echo 
 done
+
+# Jump back to the start directory
+cd $OLD_PATH
+
