6c7ac916a6ae96423ba52d1f319ab8134df371d1
Dani Teubl <teubl.dani@gmail.com>
2017-09-10 21:26:51 +0200

    backup and copy every file

--- setup.sh
+++ setup.sh
@@ -7,20 +7,30 @@ BACKUP_DIR=./backup_old_configs
 # Create temporary diretory for the old configuration files
 mkdir -p $BACKUP_DIR
 
-LINKED_FILE=vimrc
-OLD_FILE=~/.$LINKED_FILE
+# Create a backup from every config file which will be modified
+for file in *
+do
+    if [[ -f $file ]]; then
+        echo $file
 
-# Link the vimrc, but create a backup before
-if [ -f $OLD_FILE ]; then
-    echo "There is already a vimrc @HOME!"
-    if [ -L $OLD_FILE ]; then
-        echo "It is also a symlink!"
-        var=`ls -l $OLD_FILE | gawk '{ print $11}' | gawk '{ gsub (/[\/.]/, "");print}'`
-        cp $OLD_FILE "./backup_old_configs/vimrc_old_at_$var"
-    else
-        cp $OLD_FILE "./backup_old_configs/vimrc_old"
+        OLD_FILE=~/.$file
+
+        # Link the vimrc, but create a backup before
+        if [ -f $OLD_FILE ]; then
+            echo "There is already a vimrc @HOME!"
+            if [ -L $OLD_FILE ]; then
+                echo "It is also a symlink!"
+                var=`ls -l $OLD_FILE | gawk '{ print $11}' | gawk '{ gsub (/[\/.]/, "");print}'`
+                cp $OLD_FILE "./backup_old_configs/vimrc_old_at_$var"
+            else
+                cp $OLD_FILE "./backup_old_configs/vimrc_old"
+            fi
+
+            # Remove the old config file
+            # rm $file
+        fi
+        
+        # create a symbolic link to the ~/directory
+        # ln -s $file $OLD_FILE
     fi
-fi
-
-# ln -s ./vimrc $OLD_FILE
-
+done
