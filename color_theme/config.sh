26964f3b1a6d0ee3d065f6b387cf3da2b48a683e
Dani Teubl <teubl.dani@gmail.com>
2018-01-07 12:55:46 +0100

    Compile vim and build ycm core from source

--- color_theme/config.sh
+++ color_theme/config.sh
@@ -5,4 +5,12 @@ echo $PWD
 cd ./color_theme
 echo $PWD
 
-cp -v ./console_custom.vim /usr/share/vim/vim80/colors/console_custom.vim
+cp -v ./console_custom.vim /usr/local/share/vim/vim80/colors/console_custom.vim
+
+# Also note that you should change the following line in the original airline dark theme:
+# 
+# Simply change the 35-th line to the followign one:
+# let s:N3   = [ '#9cffd3' , '#202020' , 85  , 235 ]
+
+
+
