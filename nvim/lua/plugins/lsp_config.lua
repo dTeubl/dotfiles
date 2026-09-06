b9b1ed8b2cfd1801f6c15bf284b2d43e4e449701
DTeubl @e5470 <teubl.dani@gmail.com>
2026-09-06 13:12:04 +0200

    nvim: plugin updates

--- nvim/lua/plugins/lsp_config.lua
+++ nvim/lua/plugins/lsp_config.lua
@@ -1,15 +1,64 @@
 return {
-    -- lspconfig
-    "neovim/nvim-lspconfig",
-    config = function()
+    -- Mason for managing LSP binaries
+    {"williamboman/mason.nvim", cmd = "Mason", opts = {}},
 
-        -- require("lspconfig").pyright.setup {capabilities = capabilities}
+    {
+        "williamboman/mason-lspconfig.nvim",
+        opts = {ensure_installed = {"texlab"}}
+    }, -- Main LSP Configuration
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            "williamboman/mason.nvim", "williamboman/mason-lspconfig.nvim"
+        },
+        config = function()
+            -- Define common keymaps when an LSP attaches
+            vim.api.nvim_create_autocmd("LspAttach", {
+                group = vim.api.nvim_create_augroup("UserLspConfig",
+                                                    {clear = true}),
+                callback = function(ev)
+                    local opts = {buffer = ev.buf, silent = true}
 
-        vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename)
-        vim.keymap.set("n", "<leader>h", vim.lsp.buf.hover)
-        vim.keymap.set("n", "<leader>j", vim.lsp.buf.definition)
-        vim.keymap.set("n", "<leader>c", vim.lsp.buf.incoming_calls)
-        vim.keymap.set("n", "<leader>o", vim.lsp.buf.outgoing_calls)
-        vim.keymap.set("n", "<leader>f", vim.lsp.buf.references)
-    end
+                    vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename)
+                    vim.keymap.set("n", "<leader>h", vim.lsp.buf.hover)
+                    vim.keymap.set("n", "<leader>c", vim.lsp.buf.incoming_calls)
+                    vim.keymap.set("n", "<leader>o", vim.lsp.buf.outgoing_calls)
+                    vim.keymap.set("n", "<leader>f", vim.lsp.buf.references)
+                    vim.keymap.set("n", "<leader>v", vim.diagnostic.open_float)
+
+                end
+            })
+
+            vim.lsp.config("texlab", {
+                settings = {
+                    texlab = {
+                        -- Build settings (uses latexmk)
+                        build = {
+                            executable = "latexmk",
+                            args = {
+                                "-pdf", "-interaction=nonstopmode",
+                                "-synctex=1", "-outdir=./", "-auxdir=./build",
+                                "%f"
+                            },
+                            onSave = true,
+                            forwardSearchAfter = false
+                        },
+                        -- Forward search configuration (zathura, skim, okular, etc.)
+                        forwardSearch = {
+                            executable = "zathura",
+                            args = {"--synctex-forward", "%l:1:%f", "%p"}
+                        },
+                        -- Linting via ChkTeX
+                        chktex = {onOpenAndSave = true},
+                        -- Citation and label autocompletions
+                        completion = {cite = {enabled = true}}
+                    }
+                }
+            })
+
+            -- 3. Enable the language server globally
+            vim.lsp.enable("texlab")
+        end
+    }
 }
+
